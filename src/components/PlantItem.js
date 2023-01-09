5d8ef4b489b663c99d73addf91a42b07a8238076
Charles-Antoine-FENET <charlychandail@hotmail.com>
2023-01-09 16:38:21 +0100

    Add event onClick test

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,17 +1,23 @@
 import CareScale from "./CareScale";
 import "../styles/PlantItem.css";
 
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`);
+  }
+
+
 function PlantItem({ id, cover, name, light, water }) {
   return (
-    <li key={id} className="lmj-plant-item">
-      <img className="lmj-plant-item-cover" src={cover} alt=""></img>
+    <li className="lmj-plant-item" onClick={()=>handleClick(name)}>
+      <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
       {name}
       <div>
-        <CareScale careType="light" scaleValue={light} />
         <CareScale careType="water" scaleValue={water} />
+        <CareScale careType="light" scaleValue={light} />
       </div>
     </li>
   );
 }
+
 
 export default PlantItem;
